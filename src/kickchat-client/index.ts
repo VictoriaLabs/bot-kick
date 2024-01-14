f778a8b5762d10afd2ced9a0c5bbbb89f0817441
KAymeric <115625523+KAymeric@users.noreply.github.com>
2024-01-14 18:58:50 +0100

    feat : live chat getter

---
+++ src/kickchat-client/index.ts
@@ -0,0 +1,100 @@
+import { URLSearchParams } from "url";
+import { v4 as uuidv4 } from "uuid";
+import WebSocket from "ws";
+
+import { onMessage } from "./handlers/onMessage";
+import { getChatroomId, runtimeChannelData } from "./utils";
+
+const baseUrl = process.env.KICKCHAT_WEBSOCKET;
+const urlParams = new URLSearchParams({
+  protocol: "7",
+  client: "js",
+  version: "7.4.0",
+  flash: "false",
+});
+const url = `${baseUrl}?${urlParams.toString()}`;
+
+// limit max channels per socket (depends on events per sec received )
+const maxChannelsPerSocket = 10;
+let activeSockets: WebSocket[] = [];
+
+// Function to connect to channels on a given WebSocket connection
+const connectToChannels = async (socket: WebSocket, channels: any[]) => {
+  channels.forEach(async (channel) => {
+    const connect = JSON.stringify({
+      event: "pusher:subscribe",
+      data: { auth: "", channel: `chatrooms.${channel}.v2` },
+    });
+    await socket.send(connect);
+    const channelName = runtimeChannelData.get(channel);
+    console.log(`connected to #${channelName}`);
+  });
+};
+
+// Function to create a new WebSocket connection and connect to channels
+const createNewSocket = async (channels: number[]) => {
+  const socket = new WebSocket(url);
+  const id = uuidv4();
+  socket.on("open", () => {
+    console.log(`Connected to socket server ${id}`);
+    activeSockets.push(socket);
+    connectToChannels(socket, channels);
+  });
+  socket.on("message", (data: WebSocket.Data) => {
+    const messageEvent = data.toString();
+    onMessage(messageEvent);
+  });
+  socket.on("close", () => {
+    console.log("Disconnected from server");
+    activeSockets = activeSockets.filter((s) => s !== socket);
+  });
+};
+
+// Function to count the number of channels connected to the current socket
+const countConnectedChannels = (socket: WebSocket): number => {
+  let count = 0;
+  socket.listeners("message").forEach((listener) => {
+    const connectEvent = JSON.parse(listener.toString());
+    if (connectEvent && connectEvent.event === "pusher:subscribe") {
+      count++;
+    }
+  });
+  return count;
+};
+
+// Function to handle connecting to channels on active WebSocket connections or create a new WebSocket if needed
+const connectToDynamicChannels = async (channels: number[]) => {
+  console.log(`Connecting to ${channels.length} channels...`);
+  activeSockets.forEach((socket) => {
+    const connectedChannelCount = countConnectedChannels(socket);
+    console.log(
+      `Socket ${socket.url} is connected to ${connectedChannelCount} channels`
+    );
+  });
+  let connectedChannelCount = 0;
+  activeSockets.forEach((socket) => {
+    if (connectedChannelCount < maxChannelsPerSocket) {
+      const channelsToConnect = channels.slice(
+        connectedChannelCount,
+        maxChannelsPerSocket - connectedChannelCount
+      );
+      connectToChannels(socket, channelsToConnect);
+      connectedChannelCount += channelsToConnect.length;
+    }
+  });
+  while (connectedChannelCount < channels.length) {
+    const channelsToConnect = channels.slice(
+      connectedChannelCount,
+      connectedChannelCount + maxChannelsPerSocket
+    );
+    await createNewSocket(channelsToConnect);
+    connectedChannelCount += channelsToConnect.length;
+  }
+};
+
+const client = async (channels: string[]) => {
+  const chatroomIds = await getChatroomId(channels);
+  connectToDynamicChannels(chatroomIds);
+};
+
+export { client };
---
+++ src/kickchat-client/utils/index.ts
@@ -0,0 +1,40 @@
+import puppeteer from "puppeteer-extra";
+import StealthPlugin from "puppeteer-extra-plugin-stealth";
+
+import { KickChannelInfo } from "../types/channels";
+
+require('dotenv').config();
+
+
+// this is a runtime solution since you should be persisting this data in db
+export const runtimeChannelData = new Map<number, string>();
+
+export const getChannelData = async (channel: string) => {
+  const puppeteerExtra = puppeteer.use(StealthPlugin()) as typeof puppeteer;
+  const browser = await puppeteerExtra.launch({ headless: "new",args: ['--no-sandbox'] });
+  const page = await browser.newPage();
+  await page.goto(process.env.KICK_API+channel);
+  await page.waitForSelector("body");
+  try {
+    const jsonContent: KickChannelInfo = await page.evaluate(() => {
+      const bodyElement = document.querySelector("body");
+      const bodyText = bodyElement ? bodyElement.textContent : null;
+      return bodyText ? JSON.parse(bodyText) : null;
+    });
+    await browser.close();
+    runtimeChannelData.set(jsonContent.chatroom.id, jsonContent.slug);
+    return jsonContent;
+  } catch (err: any) {
+    throw err;
+  }
+};
+
+export const getChatroomId = async (channels: string[]) => {
+  let chatroomIds: number[] = [];
+  for (const channel of channels) {
+    const channelData = await getChannelData(channel);
+    const chatRoomId = channelData.chatroom.id;
+    chatroomIds.push(chatRoomId);
+  }
+  return chatroomIds;
+};
